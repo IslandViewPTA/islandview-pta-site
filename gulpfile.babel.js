540d0a9bd644fbce0f8314d5383e9cf59bb8f2c9
Bryan Klein <bryan@bryanklein.com>
2017-08-29 11:37:52 -0700

    Add htmltidy to build scripts.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -11,6 +11,7 @@ import webpackConfig from "./webpack.conf";
 import svgstore from "gulp-svgstore";
 import svgmin from "gulp-svgmin";
 import inject from "gulp-inject";
+import htmltidy from "gulp-htmltidy";
 import replace from "gulp-replace";
 import cssnano from "cssnano";
 
@@ -18,11 +19,13 @@ const browserSync = BrowserSync.create();
 const hugoBin = `./bin/hugo.${process.platform === "win32" ? "exe" : process.platform}`;
 const defaultArgs = ["-d", "../dist", "-s", "site"];
 
+var gulpsync = require('gulp-sync')(gulp);
+
 gulp.task("hugo", (cb) => buildSite(cb));
 gulp.task("hugo-preview", (cb) => buildSite(cb, ["--buildDrafts", "--buildFuture"]));
 
-gulp.task("build", ["css", "js", "hugo"]);
-gulp.task("build-preview", ["css", "js", "hugo-preview"]);
+gulp.task("build", gulpsync.sync(["css", "js", "hugo", "tidyhtml"]));
+gulp.task("build-preview", gulpsync.sync(["css", "js", "hugo-preview", "tidyhtml"]);
 
 gulp.task("css", () => (
   gulp.src("./src/css/*.css")
@@ -66,7 +69,15 @@ gulp.task("svg", () => {
     .pipe(gulp.dest("site/layouts/partials/"));
 });
 
-gulp.task("server", ["hugo", "css", "js", "svg"], () => {
+gulp.task('tidyhtml', function() {
+  return gulp.src('./dist/**/*.html')
+        .pipe(htmltidy({doctype: 'html5',
+                       hideComments: true,
+                       indent: true}))
+        .pipe(gulp.dest('./dist/'));;
+});
+
+gulp.task("server", gulpsync.sync(["hugo", "css", "js", "svg", "tidyhtml"]), () => {
   browserSync.init({
     server: {
       baseDir: "./dist"
